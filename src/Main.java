73ac56a1932b392827496fa74b2f7a3c8a7f0633
artem <artemkirlanovia@gmail.com>
2023-11-23 14:06:12 +0300

    update task

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ public class Main {
         int j = sortDesc(145263);
         int k = sortDesc(123456789);
         int f = sortDesc(3);
-
+        int s = sortDesc(0);
+        int g = sortDesc(-3);
     }
 
     public static int sortDesc(final int number) {
+        if (number < 0) {
+            throw new RuntimeException("Number need to be positive!");
+        }
+        if (number < 10) {
+            return number;
+        }
+
         List<Integer> arrayOfNumbers = new ArrayList<>();
         int value = number;
 
         while (value != 0) {
             arrayOfNumbers.add(value % 10);
             value /= 10;
-        }
-        if (arrayOfNumbers.size() == 1) {
-            return arrayOfNumbers.get(0);
         }
 
         Collections.sort(arrayOfNumbers);
